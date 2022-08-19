7861f02f90e965815b97f371e58c973808304872
Jaime Mejia <jaimemejia1003@gmail.com>
2022-08-19 11:55:52 -0500

    M sync.sh

--- sync.sh
+++ sync.sh
@@ -1,19 +1,3 @@
-#!bin/bash
-
-# export PATH=$PATH:/usr/local/bin
-
-# gstatus = `git status --porcelain`
-
-# if [ ${#gstatus} -ne 0 ]
-# then
-#     git add --all
-#     sleep 10
-#     git commit -m "Automated sync: $gstatus"
-#     sleep 10
-#     git pull --rebase
-#     sleep 10
-#     git push
-# fi
 #!/bin/bash
 
 gstatus=`git status --porcelain`
