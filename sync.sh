21b92d861e4c626b148c8b645a100196f4c69a80
Jaime Mejia <jaimemejia1003@gmail.com>
2022-08-19 11:59:25 -0500

    Automated sync:  M sync.sh

--- sync.sh
+++ sync.sh
@@ -5,11 +5,8 @@ gstatus=`git status --porcelain`
 if [ ${#gstatus} -ne 0 ]
 then
     git add --all
-    sleep 10
     git commit -m "Automated sync: $gstatus"
-    sleep 10
     git pull --rebase
-    sleep 10
     git push
 fi
 #!/bin/bash
